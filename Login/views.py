9286f52862804d59483479393f08535e58eb533b
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-07-04 11:42:03 -0400

    Teste depois do desenvolvimento (my bad)  da autenticação de funcionário

--- Login/views.py
+++ Login/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
-from models import Funcionario
 
 def login_de_funcionario(requisicao):
 	return render(requisicao, "login.html")
