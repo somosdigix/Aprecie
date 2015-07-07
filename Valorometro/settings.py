b6eac520174f4240b43b234abfd9c6807fcd4421
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-07-06 22:47:41 -0400

    Alterações pra produção

--- Valorometro/settings.py
+++ Valorometro/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
 AUTH_USER_MODEL = 'Login.Funcionario'
 AUTHENTICATION_BACKENDS = ('Login.backend.FuncionarioBackend', )
 LOGIN_URL = '/login/'
+ON_OPENSHIFT = "OPENSHIFT_APP_NAME" in os.environ
--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'Valorometro'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Valorometro.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Valorometro.release_settings'
 
 application = get_wsgi_application()
