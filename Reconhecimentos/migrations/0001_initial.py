f4d814cad92db9b13b99d0d4430a9b1ad9beef44
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-07-07 18:41:24 -0400

    Alterações no funcionario

--- Reconhecimentos/migrations/0001_initial.py
+++ Reconhecimentos/migrations/0001_initial.py
@@ -2,13 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('Login', '0001_initial'),
     ]
 
     operations = [
@@ -17,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('justificativa', models.CharField(max_length=200)),
-                ('funcionario', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('funcionario', models.ForeignKey(to='Login.Funcionario')),
             ],
         ),
         migrations.CreateModel(
