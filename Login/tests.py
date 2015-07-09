35458edaae15c359cff16f827263b355f66a2e5a
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2015-07-08 22:24:06 -0400

    Alteração dos testes da forma de login

--- Login/tests.py
+++ Login/tests.py
@@ -1,24 +1,33 @@
+#-*- coding: utf-8 -*-
 from django.test import TestCase
 from datetime import datetime
-from django.contrib.auth import authenticate
 from Login.models import Funcionario
+from django.core.urlresolvers import reverse
+import json
 
 class TesteDeAutenticacao(TestCase):
 
 	def testa_autenticacao_de_funcionario_inexistente(self):
 		cpf = "um cpf qualquer"
-		data_de_nascimento = datetime.today()
+		data_de_nascimento = '01/01/2001'
+		dados_da_requisicao = dict(cpf=cpf, data_de_nascimento=data_de_nascimento)
 
-		funcionario = authenticate(cpf=cpf, data_de_nascimento=data_de_nascimento)
+		resposta = self.client.post(reverse('entrar'), dados_da_requisicao)
+		
+		resposta_json = json.loads(resposta.content)
+		self.assertFalse(resposta_json['autenticado'])
+		self.assertEqual(u'Colaborador não encontrado, confirme seus dados tente novamente', resposta_json['mensagem'])
 
-		self.assertIsNone(funcionario)
 
 	def testa_autenticacao_de_funcionario_existente(self):
 		nome = "Guilherme Barbosa Ferreira"
 		data_de_nascimento = datetime(1991, 3, 16)
 		cpf = "16457244335"
 		funcionario = Funcionario.objects.create(cpf=cpf, data_de_nascimento=data_de_nascimento, nome=nome)
+		dados_da_requisicao = dict(cpf=cpf, data_de_nascimento=data_de_nascimento.strftime("%d/%m/%Y"))
 
-		funcionario_autenticado = authenticate(cpf=cpf, data_de_nascimento=data_de_nascimento)
+		resposta = self.client.post(reverse('entrar'), dados_da_requisicao)
 
-		self.assertEqual(funcionario, funcionario_autenticado)
+		resposta_json = json.loads(resposta.content)
+		self.assertTrue(resposta_json['autenticado'])
+		self.assertEqual(funcionario.id, resposta_json['id_do_colaborador'])
