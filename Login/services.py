dce0b0e1f3f8f3b67b02e2d8698dca3232ef158e
Renan Siravegna <renan.siravegna@gmail.com>
2015-07-10 09:17:39 -0400

    Refatorando serviço de autenticação

--- Login/services.py
+++ Login/services.py
@@ -4,7 +4,9 @@ from Login.models import Funcionario
 class ServicoDeAutenticacao():
 
 	def autenticar(self, cpf, data_de_nascimento):
-		if not Funcionario.objects.filter(cpf=cpf, data_de_nascimento=data_de_nascimento).exists():
+		funcionario = Funcionario.objects.filter(cpf=cpf, data_de_nascimento=data_de_nascimento)
+
+		if not funcionario:
 			raise Exception('Colaborador não encontrado, confirme seus dados e tente novamente')
 
-		return Funcionario.objects.get(cpf=cpf, data_de_nascimento=data_de_nascimento)
+		return funcionario[0]
