054ad405647789bf32d9c0d3ac98b92bd1917e66
Renan Siravegna <renan.siravegna@gmail.com>
2015-07-09 21:08:55 -0400

    Colocando timer no growl

--- static/app/helpers/growl.js
+++ static/app/helpers/growl.js
@@ -4,15 +4,24 @@ define([
 	'use strict';
 	
 	var growl = {};
+	var timer;
 
 	growl.exibir = function(mensagem) {
 		$('div[data-js="growl"] span').text(mensagem);
 		$('div[data-js="growl"]').show();
+
+		reiniciarTimer();
 	};
 
 	growl.esconder = function() {
 		$('div[data-js="growl"]').hide();
 	};
+
+	function reiniciarTimer() {
+		clearTimeout(timer);
+
+		timer = setTimeout(growl.esconder, 3000);
+	}
 
 	return growl;
 });
