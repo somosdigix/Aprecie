2b41589f4391dbcf91dc4f75efb608561d39a202
Renan Siravegna <renan.siravegna@gmail.com>
2015-09-06 22:35:39 -0400

    Convertendo scss para sass

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-	gulp.src('css/**/*.scss')
+	gulp.src('css/**/*.sass')
 		.pipe(sass({
-			indentedSyntax: false
+			indentedSyntax: true
 		}).on('error', sass.logError))
 		.pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function() {
-	gulp.watch('css/**/*.scss', ['sass']);
+	gulp.watch('css/**/*.sass', ['sass']);
 });
