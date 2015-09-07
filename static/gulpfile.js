fe301ea7aa0bf0c2dacf08dcc77af6d027839769
Renan Siravegna <renan.siravegna@gmail.com>
2015-09-06 22:03:28 -0400

    Temporáriamente colocando o css no controle de versão para tentar eliminar a dependência com Ruby

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-	gulp.src('css/**/*.sass')
-		.pipe(sass().on('error', sass.logError))
+	gulp.src('css/**/*.scss')
+		.pipe(sass({
+			indentedSyntax: false
+		}).on('error', sass.logError))
 		.pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function() {
-	gulp.watch('css/**/*.sass', ['sass']);
+	gulp.watch('css/**/*.scss', ['sass']);
 });
