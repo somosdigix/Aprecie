cfcf5724a51868a4855314d576c3024083836019
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2016-01-13 17:33:07 -0300

    Melhorando suporte para outros tipos de imagem

--- Login/views.py
+++ Login/views.py
@@ -7,6 +7,7 @@ import base64
 from PIL import Image
 import os
 from django.conf import settings
+import re
 
 def entrar(requisicao):
 	cpf = requisicao.POST['cpf']
@@ -29,22 +30,32 @@ def alterar_foto(requisicao):
 
 	return JsonResponse({})
 
+def obter_imagem(colaborador):
+	if colaborador.foto:
+		padrao = r'^data:image/.+;base64,(?P<b64>.+)'
+		match = re.match(padrao, colaborador.foto)
+		b64 = match.group('b64')
+		image = Image.open(BytesIO(base64.b64decode(b64)))
+	else:
+		caminho_da_foto_padrao = os.path.join(settings.STATICFILES_DIRS[0], "img", "sem-foto.png")
+		image = Image.open(caminho_da_foto_padrao)
+
+	return image
+
 def foto_do_perfil(requisicao, id_do_colaborador):
 	# TODO: Tratar outras extensoes de imagem
 	eh_miniatura = int(requisicao.GET['eh_miniatura'])
 	colaborador = Funcionario.objects.get(pk=id_do_colaborador)
-	
-	if colaborador.foto:
-		dados = colaborador.foto.replace('data:image/png;base64,', '')
-		image = Image.open(BytesIO(base64.b64decode(dados)))
-	else:
-		image = Image.open(os.path.join(settings.STATICFILES_DIRS[0], "img", "sem-foto.png"))
+
+	image = obter_imagem(colaborador)
+	extensao = image.format
 
 	if eh_miniatura:
 		image.thumbnail((50,50))
 
-	response = HttpResponse(content_type="image/png")
-	image.save(response, "png")
+	response = HttpResponse(content_type="image/{0}".format(extensao))
+
+	image.save(response, extensao)
 	return response
 
 def obter_funcionarios(requisicao):
