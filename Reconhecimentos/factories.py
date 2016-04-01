516094866ef0e77e1fc2e5ef785b77bf9c748b1d
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2016-04-01 18:28:47 -0400

    Melhoria na separação de models (DescricaoDoValor)

--- Reconhecimentos/factories.py
+++ Reconhecimentos/factories.py
@@ -1,6 +1,34 @@
 import factory
 from Login.factories import ColaboradorFactory
-from Reconhecimentos.models import Reconhecimento, Valor
+from Reconhecimentos.models import Reconhecimento, Valor, DescricaoDoValor
+
+
+class DescricaoDoValorFactory(factory.django.DjangoModelFactory):
+	class Meta:
+		model = DescricaoDoValor
+
+	descricao = factory.Iterator([
+		"Indivíduos e interação entre eles mais que processos e ferramentas",
+		"Software em funcionamento mais que documentação abrangente",
+		"Colaboração com o cliente mais que negociação de contratos",
+		"Responder a mudanças mais que seguir um plano"])
+
+class ValorFactory(factory.django.DjangoModelFactory):
+
+	class Meta:
+		model = Valor
+
+	nome = "Agilidade"
+	resumo = "bla bla bla"
+
+	@factory.post_generation
+	def descricoes(self, create, extracted, **kwargs):
+		if not create:
+			return
+
+		if extracted:
+			for descricao in extracted:
+				self.descricoes.add(descricao)
 
 class ReconhecimentoFactory(factory.django.DjangoModelFactory):
 
