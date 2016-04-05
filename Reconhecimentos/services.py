fa475bb8b33689cae27ef5339851da4a5ab3770a
Renan Siravegna <renan.siravegna@gmail.com>
2016-04-05 14:09:04 -0400

    Corrigindo a mensagem que estava invertendo reconhecido com reconhecedor

--- Reconhecimentos/services.py
+++ Reconhecimentos/services.py
@@ -7,7 +7,7 @@ class Notificacoes():
 		if not settings.ON_OPENSHIFT or not reconhecedor.usuario_no_slack or not reconhecido.usuario_no_slack:
 			return
 
-		mensagem = '*{0}* acabou de ser reconhecido em *{1}* por *{2}*'.format(reconhecedor.usuario_no_slack, valor.nome, reconhecido.usuario_no_slack)
+		mensagem = '*{0}* acabou de ser reconhecido em *{1}* por *{2}*'.format(reconhecido.usuario_no_slack, valor.nome, reconhecedor.usuario_no_slack)
 		
 		slack = slackweb.Slack(url='https://hooks.slack.com/services/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')
 		slack.notify(text=mensagem, channel='#geral', username='Aprecie.me')
