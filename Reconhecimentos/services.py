fab453eaa139569543ba98eeece3b0a3c7ce619e
Yuri Claure <yuriclaure@gmail.com>
2016-04-06 10:52:02 -0400

    Adicionando link pros mentions no slack.

--- Reconhecimentos/services.py
+++ Reconhecimentos/services.py
@@ -10,4 +10,4 @@ class Notificacoes():
 		mensagem = '*@{0}* acabou de ser reconhecido em *{1}* por *@{2}*'.format(reconhecido.usuario_no_slack, valor.nome, reconhecedor.usuario_no_slack)
 		
 		slack = slackweb.Slack(url='https://hooks.slack.com/services/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')
-		slack.notify(text=mensagem, channel='#geral', username='Aprecie.me')
+		slack.notify(text=mensagem, channel='#geral', username='Aprecie.me', link_names=1)
