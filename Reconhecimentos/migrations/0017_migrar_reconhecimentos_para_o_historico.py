fafd94d3d833798c281a7e614d0fdf89d091f021
Renan S Moreira <renan.siravegna@gmail.com>
2016-10-01 16:03:59 -0400

    Voltando toolbar no front-end e criando migrations para levar os reconhecimentos para o histórico

---
+++ Reconhecimentos/migrations/0017_migrar_reconhecimentos_para_o_historico.py
@@ -0,0 +1,24 @@
+﻿# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models, migrations
+
+def migrar_reconhecimentos(apps, schema_editor):
+  Reconhecimento = apps.get_model('Reconhecimentos', 'Reconhecimento')
+  ReconhecimentoHistorico = apps.get_model('Reconhecimentos', 'ReconhecimentoHistorico')
+
+  for reconhecimento in Reconhecimento.objects.all():
+    reconhecimento_historico = ReconhecimentoHistorico.objects.create(reconhecedor = reconhecimento.reconhecedor,
+      reconhecido = reconhecimento.reconhecido, feedback = reconhecimento.feedback, valor = reconhecimento.valor,
+      data = reconhecimento.data)
+
+    reconhecimento_historico.save()
+
+class Migration(migrations.Migration):
+
+  dependencies = [
+    ('Reconhecimentos', '0016_reconhecimentohistorico'),
+  ]
+
+  operations = [
+    migrations.RunPython(migrar_reconhecimentos)
+  ]
--- Reconhecimentos/models.py
+++ Reconhecimentos/models.py
@@ -1,35 +1,35 @@
-import django
+﻿import django
 from datetime import date
 from django.db import models
 from Aprecie.base import ExcecaoDeDominio
 
 class DescricaoDoValor(models.Model):
- 	descricao = models.CharField(max_length=100)
+  descricao = models.CharField(max_length=100)
 
 class Valor(models.Model):
-	nome = models.CharField(max_length=200)
-	resumo = models.CharField(max_length=100)
-	descricoes = models.ManyToManyField(DescricaoDoValor)
+  nome = models.CharField(max_length=200)
+  resumo = models.CharField(max_length=100)
+  descricoes = models.ManyToManyField(DescricaoDoValor)
 
-	@property
-	def frases_de_descricao(self):
-		return list(self.descricoes.values_list('descricao', flat=True))
+  @property
+  def frases_de_descricao(self):
+    return list(self.descricoes.values_list('descricao', flat=True))
 
 class Reconhecimento(models.Model):
-	reconhecedor = models.ForeignKey('Login.Colaborador', related_name='reconhecedor')
-	reconhecido = models.ForeignKey('Login.Colaborador', related_name='reconhecido')
-	feedback = models.ForeignKey('Feedback', related_name='feedback')
-	valor = models.ForeignKey(Valor)
-	data = models.DateField(auto_now_add=True)
+  reconhecedor = models.ForeignKey('Login.Colaborador', related_name='reconhecedor')
+  reconhecido = models.ForeignKey('Login.Colaborador', related_name='reconhecido')
+  feedback = models.ForeignKey('Feedback', related_name='feedback')
+  valor = models.ForeignKey(Valor)
+  data = models.DateField(auto_now_add=True)
 
-	def alterar_feedback(self, novo_feedback, reconhecedor):
-		if self.reconhecedor != reconhecedor:
-			raise ExcecaoDeDominio('O reconhecimento só pode ser alterado por quem o elaborou')
-			
-		if self.data != date.today():
-			raise ExcecaoDeDominio('O reconhecimento só pode ser alterado no mesmo dia de sua realização')
+  def alterar_feedback(self, novo_feedback, reconhecedor):
+    if self.reconhecedor != reconhecedor:
+      raise ExcecaoDeDominio('O reconhecimento só pode ser alterado por quem o elaborou')
+      
+    if self.data != date.today():
+      raise ExcecaoDeDominio('O reconhecimento só pode ser alterado no mesmo dia de sua realização')
 
-		self.feedback = novo_feedback
+    self.feedback = novo_feedback
 
 class ReconhecimentoHistorico(models.Model):
   reconhecedor = models.ForeignKey('Login.Colaborador', related_name='reconhecedor_historico')
@@ -39,9 +39,9 @@ class ReconhecimentoHistorico(models.Model):
   data = models.DateField()
 
 class Feedback(models.Model):
-	situacao = models.CharField(max_length=1000)
-	comportamento = models.CharField(max_length=1000)
-	impacto = models.CharField(max_length=1000)
+  situacao = models.CharField(max_length=1000)
+  comportamento = models.CharField(max_length=1000)
+  impacto = models.CharField(max_length=1000)
 
-	def __eq__(self, other):
-		return self.situacao == other.situacao and self.comportamento == other.comportamento and self.impacto == other.impacto
+  def __eq__(self, other):
+    return self.situacao == other.situacao and self.comportamento == other.comportamento and self.impacto == other.impacto
