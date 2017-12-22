65f2331908b84a135843f4ad0e25d146e6b4024d
Renan S Moreira <renan.siravegna@gmail.com>
2017-12-22 17:08:13 -0300

    Testando configuração de páginas estáticas

--- Aprecie/settings.py
+++ Aprecie/settings.py
@@ -75,13 +75,9 @@ TIME_ZONE = 'America/Campo_Grande'
 
 USE_TZ = False
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
-STATIC_URL = '/static/'
+STATIC_URL = '/'
 
 ON_OPENSHIFT = "OPENSHIFT_APP_NAME" in os.environ
 
