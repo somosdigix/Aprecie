ebde93ab7d6a8d5ff7489601c874c733ae0852dc
AleeLouffer <aleelouffer@gmail.com>
2021-12-14 14:41:38 -0400

    Criando e peenchendo o modal

--- static/app/perfil/apreciacoesPrint.js
+++ static/app/perfil/apreciacoesPrint.js
@@ -1,31 +1,72 @@
+function obterApreciacao(event, pilar, descricao, reconhecedor, reconhecido) {
+   var apreciacao = {
+     pilar : pilar,
+     reconhecido : reconhecido,
+     reconhecedor : reconhecedor,
+     descricao : descricao
+   };
+
+   preencherModal(apreciacao);
+}
+
+function preencherModal(apreciacao){
+  verificarPilar(apreciacao.pilar);
+
+  document.querySelector(".card--titulo").innerHTML = apreciacao.reconhecido;
+
+  document.querySelector(".card--mensagem").innerHTML = apreciacao.descricao;
+
+  document.querySelector(".card--mensagemDe").innerHTML = apreciacao.reconhecedor;
+  
+  gerarModal();
+}
+
+function verificarPilar(pilar) {
+	var backgroundDiv = document.getElementById("card");
+
+	if (pilar === "Colaborar sempre") {
+		backgroundDiv.style.backgroundImage =
+			"url('../static/img/pilares/colaborarSempre.png')";
+	} else if (pilar === "Fazer diferente") {
+		backgroundDiv.style.backgroundImage =
+			"url('../static/img/pilares/fazerDiferente.png')";
+	} else if (pilar === "Focar nas pessoas") {
+		backgroundDiv.style.backgroundImage =
+			"url('../static/img/pilares/focarNasPessoas.png')";
+	} else if (pilar === "Planejar, entregar e aprender") {
+		backgroundDiv.style.backgroundImage =
+			"url('../static/img/pilares/planejar.png')";
+	}
+}
+
 function sumirModal() {
-    document.getElementById("modal").style.visibility = "hidden";
-    document.getElementById("corpo-modal").style.visibility = "hidden";
-  }
+	document.getElementById("modal").style.visibility = "hidden";
+	document.getElementById("corpo-modal").style.visibility = "hidden";
+}
 
-  function gerarModal() {
-    document.getElementById("modal").style.visibility = "visible";
-    document.getElementById("corpo-modal").style.visibility = "visible";
-  }
+function gerarModal() {
+	document.getElementById("modal").style.visibility = "visible";
+	document.getElementById("corpo-modal").style.visibility = "visible";
+}
 
-  function SaveAs() {
-    html2canvas(document.querySelector("#card")).then(function (canvas) {
-      var uri = canvas.toDataURL();
-      var filename = "file-name.png";
+function SaveAs() {
+	html2canvas(document.querySelector("#card")).then(function (canvas) {
+		var uri = canvas.toDataURL();
+		var filename = "file-name.png";
 
-      var link = document.createElement("a");
+		var link = document.createElement("a");
 
-      if (typeof link.download === "string") {
-        link.href = uri;
-        link.download = filename;
+		if (typeof link.download === "string") {
+			link.href = uri;
+			link.download = filename;
 
-        document.body.appendChild(link);
+			document.body.appendChild(link);
 
-        link.click();
+			link.click();
 
-        document.body.removeChild(link);
-      } else {
-        window.open(uri);
-      }
-    });
-  }
+			document.body.removeChild(link);
+		} else {
+			window.open(uri);
+		}
+	});
+}
