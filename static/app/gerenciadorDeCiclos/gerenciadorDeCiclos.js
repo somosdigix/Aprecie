e68b8e16b37eeaccf96f95e1eaf0b1269be0648a
JeanMenezees <jeanvbonimenezes@gmail.com>
2022-01-19 15:53:00 -0300

    extraindo js para exibir informações do server para os ciclo atual

--- static/app/gerenciadorDeCiclos/gerenciadorDeCiclos.js
+++ static/app/gerenciadorDeCiclos/gerenciadorDeCiclos.js
@@ -1,65 +1,117 @@
 define([
-	"jquery",
-	"template",
-	"text!app/gerenciadorDeCiclos/gerenciadorDeCiclosTemplate.html",
+    "jquery",
+    "template",
+    "text!app/gerenciadorDeCiclos/gerenciadorDeCiclosTemplate.html",
     "sessaoDeUsuario",
     "growl",
     "roteador"
-], function ($, template, gerenciadorDeCiclosTemplate, sessaoDeUsuario, growl,  roteador) {
-	"use strict";
+], function ($, template, gerenciadorDeCiclosTemplate, sessaoDeUsuario, growl, roteador) {
+    "use strict";
 
-	var self = {};
-	var _sandbox;
+    var self = {};
+    var _sandbox;
 
-	self.inicializar = function (sandbox) {
-		_sandbox = sandbox;
-        carregarGerenciador()
+    self.inicializar = function (sandbox) {
+        _sandbox = sandbox;
+
+        carregarGerenciador();
 
         // on click button html para função de definir ciclo e alterar ciclo
         $("#conteudo")
             .on("click", 'button[data-js="btn_adicionar_ciclo"]', definirCiclo)
-			.on("click", 'button[data-js="btn_alterar_ciclo"]', alterarCiclo)
-	};
+            .on("click", 'button[data-js="btn_alterar_ciclo"]', alterarCiclo)
+    };
 
-	self.finalizar = function () {
-		_sandbox.limpar("#conteudo");
-		_sandbox.removerEvento("#conteudo");
-	};
+    self.finalizar = function () {
+        _sandbox.limpar("#conteudo");
+        _sandbox.removerEvento("#conteudo");
+    };
 
-    function carregarGerenciador(){
-        $.getJSON("/reconhecimentos/obter_informacoes_ciclo_atual", function (ciclo_atual) {
-			template.exibir(gerenciadorDeCiclosTemplate, ciclo_atual);
-		});
+    async function carregarGerenciador() {
+        await $.getJSON("/reconhecimentos/obter_informacoes_ciclo_atual", function (ciclo_atual) {
+            template.exibir(gerenciadorDeCiclosTemplate, ciclo_atual);
+        });
+
+        definirPorcentagemNoTextoDaBarra();
+        definirPorcentagemNoCirculo();
     }
-	
-    function definirCiclo(){  
+
+    //funcoes para editar ciclo
+    function definirCiclo() {
         var data = {
             'nome_ciclo': $('#nome_ciclo').val(),
             'data_inicial': $('#data_inicial').val(),
             'data_final': $('#data_final').val(),
             'usuario_que_modificou': sessaoDeUsuario.id
         }
-        
+
         $.post('/reconhecimentos/definir_ciclo/', data, function () {
-			growl.deSucesso().exibir('Ciclo adicionado com sucesso');
-			roteador.atualizar();
-		})
+            growl.deSucesso().exibir('Ciclo adicionado com sucesso');
+            roteador.atualizar();
+        })
     }
 
-    function alterarCiclo(){
+    function alterarCiclo() {
         var data = {
             'id_ciclo': $('#id_ciclo').val(),
             'data_final': $('#nova__data__final').val(),
-            'usuario_que_modificou': sessaoDeUsuario.id ,
+            'usuario_que_modificou': sessaoDeUsuario.id,
             'descricao_da_alteracao': $('#input__motivo').val()
         }
 
         $.post('/reconhecimentos/alterar_ciclo/', data, function () {
-			growl.deSucesso().exibir('Ciclo alterado com sucesso');
-			roteador.atualizar();
-		})
+            growl.deSucesso().exibir('Ciclo alterado com sucesso');
+            roteador.atualizar();
+        })
     }
 
+    //funcoes para o ciclo atual
+    function calcularPeriodoCiclo(dataFinal, dataInicial) {
+        const diferencaDeDiasDoCiclo = Math.abs(dataFinal - dataInicial);
+        return Math.ceil(diferencaDeDiasDoCiclo / (1000 * 60 * 60 * 24));
+    }
 
-	return self;
+    function calcularProgressoEmDias(dataFinal, dataHoje) {
+        const diferencaDeDiasDoProgresso = Math.abs(dataFinal - dataHoje);
+        return Math.ceil(diferencaDeDiasDoProgresso / (1000 * 60 * 60 * 24));
+    }
+
+    function calcularPorcentagemDoProgresso() {
+        //pega do banco
+        const dataInicial = new Date('1/1/2022');
+        const dataFinal = new Date('2/20/2022');
+
+        const dataHoje = new Date();
+
+        const diferencaPeriodoEProgresso = Math.abs(
+            calcularPeriodoCiclo(dataFinal, dataInicial) - calcularProgressoEmDias(dataFinal, dataHoje)
+        );
+        return (diferencaPeriodoEProgresso / calcularPeriodoCiclo(dataFinal, dataInicial)) * 100;
+    }
+
+    function definirPorcentagemNoTextoDaBarra() {
+        $('#porcentagem').html(calcularPorcentagemDoProgresso().toFixed(0) + "%");
+    }
+
+    function definirPorcentagemNoCirculo() {
+        $("#circulo2").css("stroke-dashoffset", 314 - (calcularPorcentagemDoProgresso() / 100) * 314);
+    }
+
+    //funcoes para abrir e fechar modal
+    function mostrarContainerNovoCiclo() {
+        document.getElementById("container__novo__ciclo").style.display = "block";
+    }
+    function fecharContainerNovoCiclo() {
+        document.getElementById("container__novo__ciclo").style.display = "none";
+    }
+    function fecharModal() {
+        document.getElementById("abrirModal").style.opacity = "0";
+        document.getElementById("abrirModal").style.pointerEvents = "none";
+    }
+    function mostrarModal() {
+        document.getElementById("abrirModal").style.opacity = "1";
+        document.getElementById("abrirModal").style.pointerEvents = "auto";
+    }
+
+    return self;
 });
