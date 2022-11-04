67626c0e1dc671778729b2bc68ff1117ac862e08
Kaio Rodrigues Saravy <kaiorodriguessaravy@gmail.com>
2022-11-04 15:09:07 -0400

    att com validação

--- static/app/cadastroDeColaboradores/cadastroDeColaboradores.js
+++ static/app/cadastroDeColaboradores/cadastroDeColaboradores.js
@@ -1,6 +1,7 @@
 define([
+	"jquery",
 	'text!app/cadastroDeColaboradores/formularioTemplate.html'
-], function(cadastroTemplate) {
+], function($,cadastroTemplate) {
 	'use strict';
 
 	var self = {};
@@ -10,12 +11,48 @@ define([
 		_sandbox = sandbox;
 
 		_sandbox.exibirTemplateEm('#conteudo', cadastroTemplate);
+		$("#conteudo").on("click", 'button[data-js="SalvarColaborador"]', validarCpf)
+		$('#cpf').inputmask('999.999.999-99')
+		$('#conteudo')
+        .on('click', 'button[data-js="SalvarColaborador"]', validaFormulario)
 	};
 
 	self.finalizar = function() {
 		_sandbox.limpar('#conteudo');
 		_sandbox.removerEvento('#conteudo');
 	};
+
+	function validaFormulario(){
+		if(valida($('#cpf').val())){
+			console.log("Cpf ok");
+		}
+		else{
+			console.log("Cpf não ok");
+		}
+	}
+	
+	function valida(strCPF) {
+		strCPF = strCPF.replaceAll('.', '').replace('-','');
+		console.log(strCPF);
+		var Soma;
+		var Resto;
+		Soma = 0;
+	  if (strCPF == "00000000000") return false;
+	
+	  for (var i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+	  Resto = (Soma * 10) % 11;
+	
+		if ((Resto == 10) || (Resto == 11))  Resto = 0;
+		if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+	
+	  Soma = 0;
+		for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+		Resto = (Soma * 10) % 11;
+	
+		if ((Resto == 10) || (Resto == 11))  Resto = 0;
+		if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+		return true;
+	}
 
     return self;
 });
