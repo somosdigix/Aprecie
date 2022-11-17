aa84597e94d0bdbd8cf11ebec9d6c2baaa2f026e
Tiago Andrade <tiago22900@gmail.com>
2022-11-17 14:19:39 -0400

    adição da barragem do acesso a tela de cadastro

--- static/app/cadastroDeColaboradores/cadastroDeColaboradores.js
+++ static/app/cadastroDeColaboradores/cadastroDeColaboradores.js
@@ -11,72 +11,41 @@ define([
 
 	self.inicializar = function (sandbox) {
 		_sandbox = sandbox;
-		_sandbox.exibirTemplateEm('#conteudo', cadastroTemplate);
-		$('#conteudo').on('focusout', 'input[id="idDiscord"]', validarUserIdDiscord);
-		$("#conteudo").on("focusout", 'input[id="cpf"]', validaCPF);
-		$("#conteudo").on("focusout", 'input[id="dataDeNascimento"]', validardataDeNascimento);
-		$("#salvarColaborador").click(function (event) {
-			event.preventDefault();
-			salvarColaborador();
-		});
-		$('#cpf').inputmask('999.999.999-99');
-	};
-
-	function salvarColaborador() {
-		if (validaFormulario()) {
-			var colaboradores = [
-				{
-					cpf: $('#cpf').val(),
-					nome: $('#nomeColaborador').val(),
-					data_de_nascimento: $('#dataDeNascimento').val(),
-					usuario_id_do_chat: $('#idDiscord').val(),
-				}
-			]
-			var dados = JSON.stringify({ 'colaboradores': colaboradores })
-
-			$.post("/login/colaborador/", dados,
-				function (retorno) {
-
-					if (retorno.contagem_de_inclusoes == 0 && retorno.cpfs_invalidos.length == 0) {
-						mensagem = "Colaborador já existe.";
-						growl.deErro().exibir(mensagem);
-
-					} else if (retorno.contagem_de_inclusoes == 0 && retorno.cpfs_invalidos.length == 1) {
-						mensagem = "CPF inválido."
-						growl.deErro().exibir(mensagem);
-					}
-					else {
-						var mensagem = "Colaborador cadastrado com sucesso.";
-						growl.deSucesso().exibir(mensagem);
-					}
-
-				}).fail(function () {
-					growl.deErro().exibir("Colaborador não cadastrado.");
-				});
+		if (recursosHumanosHelper.ehRecursosHumanos()) {
+			_sandbox.exibirTemplateEm('#conteudo', cadastroTemplate);
+			$('#conteudo').on('focusout', 'input[id="idDiscord"]', validarUserIdDiscord);
+			$('#cpf').inputmask('999.999.999-99');
+			$('#conteudo')
+				.on('click', 'button[data-js="SalvarColaborador"]', validaFormulario);
+		} else {
+			roteador.navegarPara('/paginaInicial');
 		}
-
-	}
+	};
 
 	function validarUserIdDiscord() {
 		var userIdDiscord = $('#idDiscord').val();
-		var mensagem = $('#alert-discord');
+		var chaveDiscord = ''
 		$.ajax({
-			type: "GET",
-			dataType: "json",
-			url: '/login/usario_discord/' + userIdDiscord,
-			success: function (data) {
-				if (data.status == 200) {
-					mensagem.html('Esse id pertence ao usuário <strong>' + data.username + '</strong>.');
-					mensagem.removeClass("erro")
-					mensagem.addClass("sucesso")
-				} else {
-					mensagem.html("Esse id nao pertence a um usuário do discord.");
-					mensagem.removeClass("sucesso")
-					mensagem.addClass("erro")
-				}
+			beforeSend: function (request) {
+				request.setRequestHeader("Authorization", 'Bot' + chaveDiscord);
 			},
+			dataType: "json",
+			url: 'https://discord.com/api/v10/users/' + userIdDiscord,
+			success: function (data) {
+				// Se deu certo
+			},
+			statusCode: {
+				404: function () {
+					// Se deu errado
+				}
+			}
 		});
 	}
+
+	self.finalizar = function () {
+		_sandbox.limpar('#conteudo');
+		_sandbox.removerEvento('#conteudo');
+	};
 
 	function validardataDeNascimento() {
 		var data = new Date($("#dataDeNascimento").val().replace(/-/g, '/'));
@@ -101,9 +70,8 @@ define([
 	}
 
 	function validaFormulario() {
-		var data_de_nascimento = validardataDeNascimento();
-		var cpf = validaCPF();
-		return data_de_nascimento && cpf;
+		validardataDeNascimento();
+		validaCPF();
 	}
 
 	function validaCPF() {
@@ -149,7 +117,7 @@ define([
 			rev = 0;
 		if (rev != parseInt(cpf.charAt(9)))
 			return false;
-		// Valida 2o digito
+		// Valida 2o digito	
 		add = 0;
 		for (var i = 0; i < 10; i++)
 			add += parseInt(cpf.charAt(i)) * (11 - i);
@@ -160,12 +128,7 @@ define([
 			return false;
 		return true;
 	}
-
-	self.finalizar = function () {
-		_sandbox.limpar('#conteudo');
-		_sandbox.removerEvento('#conteudo');
-	};
-
 	return self;
 }
-)
+
+);
